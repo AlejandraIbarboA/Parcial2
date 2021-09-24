7bbc014ca88c34712383599d1f0cf09418f2cb90
Santiago Álvarez <santiago.alvarezb@upb.edu.co>
2021-09-24 17:25:44 -0500

    Primera parte
    Botón alimentar PENDIENTE desaparecer sprite

---
+++ Assets/Assets/Scripts/DataSO.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "ScriptableObjects/DataSO")]
+public class DataSO : ScriptableObject
+{
+    [SerializeField] int value = 0;
+    public event UnityAction<int> OnValueChanged;
+
+    public void OnValueChange()
+    {
+        value++;
+        OnValueChanged?.Invoke(value);
+    }
+}
---
+++ Assets/Assets/Scripts/ImageFood.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ImageFood : MonoBehaviour
+{
+    [SerializeField] Image imageFood;
+    [SerializeField] Sprite foodIcon;
+    [SerializeField] DataSO channel;
+    
+    private void OnEnable()
+    {
+        channel.OnValueChanged += UpdateImage; 
+    }
+    private void OnDisable()
+    {
+        channel.OnValueChanged -= UpdateImage;
+
+    }
+    void UpdateImage(int value)
+    {
+        if(value>=1)
+        {
+            imageFood.sprite = foodIcon;
+        }
+        //imageFood.sprite = null;
+
+    }
+}
