11211b3f20f05da0717d35283a83c5fb36e860fc
Maria360 <68033327+Maria360@users.noreply.github.com>
2021-09-24 20:17:39 -0500

    State machine (#10)
    Lógica implementada , pendiente funcionamiento de botones
    
    Co-authored-by: Maria Isabel Gomez <mariai.gomezr@gmail.com>

---
+++ Assets/Assets/Scripts/GameSystem.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameSystem : StateMachine
+{
+    private void Start()
+    {
+        SetState(new IdleState(this));
+    }
+    public void OnFeedButton()
+    {
+        StartCoroutine(State.Eat());
+    }
+    public void Idle()
+    {
+        StartCoroutine(State.Idle());
+    }
+}
