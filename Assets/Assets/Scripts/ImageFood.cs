d1a95c927be4ec521b040fc3f926102db7223a9f
sebash3151 <sebash31510@gmail.com>
2021-09-24 19:29:55 -0500

    Bar Update
    Food and energy update bars, amount of food and energy and their capacity. An a script to modify that values.

--- Assets/Assets/Scripts/ImageFood.cs
+++ Assets/Assets/Scripts/ImageFood.cs
@@ -6,25 +6,20 @@ using UnityEngine.UI;
 public class ImageFood : MonoBehaviour
 {
     [SerializeField] Image imageFood;
-    [SerializeField] Sprite foodIcon;
+    [SerializeField] Sprite icon;
     [SerializeField] DataSO channel;
-    
+
     private void OnEnable()
     {
-        channel.OnValueChanged += UpdateImage; 
+        channel.OnValueChanged += UpdateImage;
     }
     private void OnDisable()
     {
         channel.OnValueChanged -= UpdateImage;
-
     }
-    void UpdateImage(int value)
-    {
-        if(value>=1)
-        {
-            imageFood.sprite = foodIcon;
-        }
-        //imageFood.sprite = null;
 
+    void UpdateImage()
+    {
+        imageFood.sprite = icon;
     }
 }
