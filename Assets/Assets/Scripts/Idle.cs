1889b614e06f4188f3a82c0ea3bb6747a2e8ca5c
Maria Isabel Gomez <mariai.gomezr@gmail.com>
2021-09-24 20:16:36 -0500

    State machine
    Lógica implementada , pendiente funcionamiento de botones

---
+++ Assets/Assets/Scripts/Idle.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class IdleState : State
+{
+    public IdleState(GameSystem gameSystem) : base(gameSystem)
+    {
+    }
+    public override IEnumerator Idle()
+    {
+        Debug.Log("IDLE");
+        yield break;
+    }
+}
---
+++ Assets/Assets/Scripts/State.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class State : MonoBehaviour
+{
+    protected  GameSystem gameSystem;
+
+    protected State(GameSystem gameSystem)
+    {
+        this.gameSystem = gameSystem;
+    }
+
+    public virtual IEnumerator Idle()
+    {
+        yield break;
+    }
+    public virtual IEnumerator Eat()
+    {
+        yield break;
+    }
+}
