902a5996090181f6e24e246497e4e75eec4c4dd5
sebash3151 <60160067+sebash3151@users.noreply.github.com>
2021-09-26 19:44:27 -0500

    Sebas interface bracnh (#20)
    * Main scene update
    
    Serializable fields updated and sprite change
    
    * music, interface and objects
    
    music added, interface with the objects and the interactions with butttons

---
+++ Assets/Assets/Scripts/ColisionInteraccion.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColisionInteraccion : MonoBehaviour
+{
+    Necesities necesities;
+    I_Interactable interact;
+
+    void Start()
+    {
+        necesities = GetComponent<Necesities>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("interact"))
+        {
+            I_Interactable interactive = other.GetComponent<I_Interactable>();
+            interactive.Interactable(necesities);
+        }
+    }
+}
