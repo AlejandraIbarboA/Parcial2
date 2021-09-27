b9bfe44a0757ed6cdecc1b1153995844b8f42ed1
sebash3151 <sebash31510@gmail.com>
2021-09-26 19:26:16 -0500

    music, interfas, new objects
    Interface implementation with objects, music principal and changes to the main scene

---
+++ Assets/Assets/Scripts/Interactive/Fruta.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fruta : MonoBehaviour, I_Interactable
+{
+    public void Interactable(Necesities necesities)
+    {
+        necesities.ModifyNecesities("food", 3);
+    }
+}
